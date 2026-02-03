800fc85b04e4daee1e22cdbc3b60f17fa1940e8f
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2026-02-03 15:00:11 -0500

    Player movement working

--- scripts/move_me.gd
+++ scripts/move_me.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
-const SPEED = 500.0
+const SPEED = 200.0
 #const JUMP_VELOCITY = -400.0
 
 
@@ -19,13 +20,27 @@ func _physics_process(delta: float) -> void:
 	var x_direction := Input.get_axis("move_left", "move_right")
 	var y_direction := Input.get_axis("move_up", "move_down")
 	
-	if x_direction:
+	#anim_sprite.play("walk_down")
+	
+	#This is some very piecemeal code. The movement is slightly cooked
+	if x_direction && !y_direction:
 		velocity.x = x_direction * SPEED
+		anim_sprite.play("walk_side")
+		
+		if x_direction == 1:
+			anim_sprite.flip_h = true
+		else:
+			anim_sprite.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	if y_direction:
 		velocity.y = y_direction * SPEED
+		
+		if y_direction == 1:
+			anim_sprite.play("walk_down")
+		else:
+			anim_sprite.play("walk_up")
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
